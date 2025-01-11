5095c41bbf507fde3856d933318f7b7d727a051c
Harsh Rakholiya <rakholiyaharsh332@gmail.com>
2025-01-11 21:22:14 +0530

    Operator File

---
+++ All_Operators/All_Arithmatic_operators.java
@@ -0,0 +1,13 @@
+class All_Arithmatic_operators{
+    public static void main(String args[])
+    {
+        int a = 10;
+        int b = 20;
+
+        System.out.println("A+B::" + (a+b));
+        System.out.println("A-B::" + (a-b));
+        System.out.println("A*B::" + (a*b));
+        System.out.println("A/B::" + (a/b));
+
+    }
+}
